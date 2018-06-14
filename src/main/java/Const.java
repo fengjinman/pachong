2377b403bc128af05e326813d0879229c9fdf9b0
fengjinman@empowerwin.com <fengjinman>
2018-06-14 11:29:54 +0800

    我的爬虫代码

---
+++ src/main/java/Const.java
@@ -0,0 +1,9 @@
+/**
+ * Created by fengjinman Administrator on 2018/6/14.
+ */
+public class Const {
+
+    public static String url = "http://map.baidu.com/detail?qt=ninf&from=housezt&detail=house&uid=";
+
+    public static String uid = "5ef5edbdc64c1bb49e9d6899";
+}
---
+++ src/main/java/TestMain.java
@@ -0,0 +1,12 @@
+/**
+ * Created by fengjinman Administrator on 2018/6/14.
+ */
+public class TestMain {
+
+
+    //最后我们运行main方法测试一下：
+    public static void main(String[] args) {
+        HouseInfo info = new Analyze().regexMain();
+        System.out.println(info);
+    }
+}
